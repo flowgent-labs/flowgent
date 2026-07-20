c3441ad3d2e71bff1267a08ac8a9103bd447ded9
Flowgent Labs <jameswong1376@gmail.com>
2026-07-21 01:02:41 +0800

    fix: IT tests issues

--- pkg/core/pkg/engine/executor/noop.go
+++ pkg/core/pkg/engine/executor/noop.go
@@ -13,8 +13,10 @@ func (e *NoopExecutor) TaskType() entities.TaskType { return entities.TaskNoop }
 
 func (e *NoopExecutor) Execute(ctx context.Context, plan *entities.ExecutionPlan, scope map[string]map[string]any) (*entities.TaskResult, error) {
 	out := map[string]any{"result": true}
-	for k, v := range plan.Input {
-		out[k] = v
+	if plan != nil {
+		for k, v := range plan.Input {
+			out[k] = v
+		}
 	}
 	return &entities.TaskResult{Output: out}, nil
 }
